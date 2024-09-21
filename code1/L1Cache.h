7f43e1349819b17b53ae758a2d860657c30141d2
stfp04 <rafaelpag04@gmail.com>
2024-09-21 14:46:29 +0100

    A

---
+++ code1/L1Cache.h
@@ -0,0 +1,42 @@
+#ifndef L1CACHE_H
+#define L1CACHE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "Cache.h"
+
+#define L1_LINES (L1_SIZE / BLOCK_SIZE)
+
+void resetTime();
+
+uint32_t getTime();
+
+/****************  RAM memory (byte addressable) ***************/
+void accessDRAM(uint32_t, uint8_t *, uint32_t);
+
+/*********************** Cache *************************/
+
+void initCache();
+void accessL1(uint32_t, uint8_t *, uint32_t);
+
+typedef struct CacheLine {
+  uint8_t Valid;
+  uint8_t Dirty;
+  uint32_t Tag;
+  uint8_t words[BLOCK_SIZE];
+} CacheLine;
+
+typedef struct Cache {
+  uint32_t init;
+  CacheLine line[L1_LINES];
+} Cache;
+
+/*********************** Interfaces *************************/
+
+void read(uint32_t, uint8_t *);
+
+void write(uint32_t, uint8_t *);
+
+#endif
