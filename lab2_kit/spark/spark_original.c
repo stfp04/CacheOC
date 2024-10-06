7b361db08acb8fa7227f8155b3049bbf86c928a2
Matt <matheus.noyaa@gmail.com>
2024-10-06 16:49:15 +0100

    commit

--- lab2_kit/spark/spark_original.c
+++ lab2_kit/spark/spark_original.c
@@ -58,7 +58,7 @@ int main() {
 
             clock_t const cycle_count = clock() - start_cycles;
             double const time_diff = get_elapsed(&start_time);
-            if(stride == 4096) {
+            if(stride == 2048) {
                 a_stride = n_iterations;
                 td_stride = time_diff;
                 t_stride = stride;
@@ -71,14 +71,13 @@ int main() {
              *****************************************************************/
 
             /* Output to stdout */
-            fprintf(stdout, "%zu\t%zu\t%lf\t%zu\n", cache_size, stride,
-                    time_diff, cycle_count);
+            fprintf(stdout, "%zu\t%zu\t%lf\t%zu\t%zu\n", cache_size, stride, time_diff, cycle_count,n_iterations);
         }
         double const total_time = get_elapsed(&t1);
         double const mean_time = (total_time / total_accesses) * 1000000000.0;
         double const mstr_time = (td_stride / a_stride) * 1000000000.0;
         fprintf(stdout, "t1 - t2 (ms): %lf\nMean Access Time (ns): %lf\n", total_time  * 1000.0, mean_time);
-        fprintf(stdout, "Stride: %zu -> t2 - t1 (ms): %lf \t Mean Access Time (ns): %lf\n", t_stride, td_stride * 1000.0, mstr_time);
+        fprintf(stdout, "Stride: %zu -> t2 - t1 (ms): %lf  Mean Access Time (ns): %lf\n", t_stride, td_stride * 1000.0, mstr_time);
     }
     return 0;
 }
