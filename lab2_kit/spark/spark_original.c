fd83002581b7907990781db7504e8f242033c5d3
Matt <matheus.noyaa@gmail.com>
2024-10-06 16:16:42 +0100

    push

--- lab2_kit/spark/spark_original.c
+++ lab2_kit/spark/spark_original.c
@@ -78,7 +78,7 @@ int main() {
         double const mean_time = (total_time / total_accesses) * 1000000000.0;
         double const mstr_time = (td_stride / a_stride) * 1000000000.0;
         fprintf(stdout, "t1 - t2: %lf\nMean Access Time (ns): %lf\n", total_time, mean_time);
-        fprintf(stdout, "Stride: %zu -> t2 - t1: %lf \t Mean Access Time (ns): %lf\n", t_stride, td_stride, mstr_time);
+        fprintf(stdout, "Stride: %zu -> t2 - t1: %lf \t Mean Access Time (ns): %lf\n", t_stride, td_stride * 1000.0, mstr_time);
     }
     return 0;
 }
